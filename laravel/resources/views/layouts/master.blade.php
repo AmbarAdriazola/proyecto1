02312677aa2db0e7eb3c26f723d16895b8c93265
Patricio Almonacid <pat.nas@outlook.es>
2022-08-25 23:22:34 -0400

    Avance Vistas

--- laravel/resources/views/layouts/master.blade.php
+++ laravel/resources/views/layouts/master.blade.php
@@ -10,26 +10,23 @@
 
 <body>
     @section('header')
+    <!--Navbar-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
+            <!--Index-->
             <a class="navbar-brand" href="/">GRUPO 1</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Nosotros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/prueba">Prueba</a></li>
+                    <!--1st Button-->
+                    <li class="nav-item"><a class="nav-link active" href="/">Inicio</a></li>
+                    <!--2nd Button-->
+                    <li class="nav-item"><a class="nav-link active" href="{{ route('producto.index') }}">Nuestros Productos</a></li>
+                    <!--Dropdown button-->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tienda</a>
+                        <a class="nav-link active dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tienda</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="{{ route('producto.index') }}">Nuestros Productos</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
                             <li><a class="dropdown-item" href="{{ route('producto.create') }}">Nuevos Productos</a></li>
-                            <!-- <li><a class="dropdown-item" href="#!">Stock Productos</a></li> -->
                             <li><a class="dropdown-item" href="{{ route('bodega.create') }}">Insertar bodega</a></li>
-                            <!-- <li><a class="dropdown-item" hr ef="#!">Vista Bodega</a></li> -->
                         </ul>
                     </li>
                 </ul>
