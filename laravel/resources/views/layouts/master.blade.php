3cb325792978eff73ea204a247081d941fd6c3d1
Patricio Almonacid <pat.nas@outlook.es>
2022-08-25 22:49:38 -0400

    Vista Welcome

--- laravel/resources/views/layouts/master.blade.php
+++ laravel/resources/views/layouts/master.blade.php
@@ -10,10 +10,9 @@
 
 <body>
     @section('header')
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!">GRUPO 1</a>
+            <a class="navbar-brand" href="/">GRUPO 1</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -25,7 +24,7 @@
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="{{ route('producto.index') }}">Nuestros Productos</a></li>
                             <li>
-                                <hr class="dropdown-divider"/>
+                                <hr class="dropdown-divider" />
                             </li>
                             <li><a class="dropdown-item" href="{{ route('producto.create') }}">Nuevos Productos</a></li>
                             <!-- <li><a class="dropdown-item" href="#!">Stock Productos</a></li> -->
@@ -33,12 +32,12 @@
                             <!-- <li><a class="dropdown-item" hr ef="#!">Vista Bodega</a></li> -->
                         </ul>
                     </li>
-                </ul>   
+                </ul>
                 <form class="d-flex">
                     <button class="btn btn-outline-dark" type="submit">
                         <a class="dropdown-item" href="http://127.0.0.1:8000/login">Iniciar Sesion</a>
                     </button>
-                    </form>
+                </form>
             </div>
         </div>
     </nav>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -11,17 +11,49 @@
             <div class="text-center">
                 <h3 class="fw-bolder">¡Bienvenido a nuestra tienda!</h3>
             </div>
-            <p class="text-muted">Esta empresa surge al ver las necesidades de las personas y
+            <br>
+            <p class="text-muted">
+                <img class="text-muted" align="right" width="400" height="300" src="https://www.65ymas.com/uploads/s1/70/93/4/bigstock-zero-waste-food-shopping-eco-256506499_1_621x621.jpeg" data-was-processed="true" />
+                Esta empresa surge al ver las necesidades de las personas y
                 comerciantes que viven y trabajan en ese sector, como es adquirir productos de la canasta
                 familiar, en este caso las frutas y verduras, aún más cuando hay poca oferta de estas a
                 precios bajos, buena calidad y sobre todo con buena atención al cliente.
+            </p>
+            <p class="text-muted">
                 Buscamos generar un impacto ambiental, en la fomentación del reciclaje
                 y cuidado del medio ambiente, incentivando el uso de las bolsas de tela para así evitar el
                 uso continuo de las bolsas de plásticos.
+                Nuestra visión es conseguir la fidelización de nuestros clientes a través de la comercialización de productos y servicio de calidad, para afianzar así un crecimiento continuo y sostenido en el mercado nacional. Consolidándonos como una empresa de referencia en el sector con un modelo de negocio propio y diferenciado de la competencia. Frutería casa del campo trabaja para producir diversidad de productos de máxima calidad, con un excelente servicio y precios competitivos.
+            </p>
+            <br>
+            <p class="text-muted" style="text-align: justify;">
+                <span>
+                    <strong>
+                        Valores
+                    </strong>
+                </span>
+            </p>
+            <p class="text-muted">
+                <span>
+                    <b>>Atención al empleado:</b> Promovemos la colaboración de nuestros trabajadores, a través de las mejores condiciones laborales que garantizan una plantilla estable, satisfecha y comprometida con los objetivos.
+                </span>
+            </p>
+            <p class="text-muted">
+                <span>
+                    <b>>Orientación al cliente:</b> Nos interesamos por satisfacer las necesidades de nuestros clientes, garantizando la máxima Calidad y Seguridad alimentaria de nuestros productos para superar sus expectativas.
+                </span>
+            </p>
+            <p class="text-muted">
+                <span>
+                    <b>>Calidad en el producto:</b> La calidad de nuestros productos es imprescindible en todas las etapas de producción para el buen funcionamiento de la empresa.
+                </span>
+            </p>
+            <p class="text-muted">
+                <span>
+                    <b>>Integridad:</b> Entre nuestros valores tenemos como bandera la honestidad, la credibilidad y la equidad.
+                </span>
             </p>
         </div>
-        
-        <img class="pull-right img-responsive thumb wp-post-image lazy loaded" width="300" height="300" src="https://www.65ymas.com/uploads/s1/70/93/4/bigstock-zero-waste-food-shopping-eco-256506499_1_621x621.jpeg" data-was-processed="true"/>
     </div>
 </div>
 </section>
